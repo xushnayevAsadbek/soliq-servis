bd5c72d438d3dfc59bd3ed741cb4693cbfbe9527
asadbek755 <xushnaevasadbek@gmail.com>
2024-10-27 22:51:59 +0500

    form section commit

--- src/component/form/form.jsx
+++ src/component/form/form.jsx
@@ -1,5 +1,5 @@
 import './form.css'
-import formImg from './img/form-img-removebg-preview.jpg';
+// import formImg from './img/form-img-removebg-preview.png';
 export const Form = () => {
 return(
     <div>
@@ -11,11 +11,22 @@ return(
     </h3>
     <p className='form-box--text'>
     Kurs haqida savollaringiz bo'lsa yoki nimani tanlashni bilmasangiz, raqamingizni qoldiring - biz barcha savollaringizga javob beramiz.
-    </p> 
-    <img src={formImg} alt="" className='form-box--img' style={{
-        backgroundColor:'#EAF4FF',
-        width:"291px"
-    }} />
+    </p>    
+    <div className='form-box--img'>
+
+    </div>
+    </div>
+    <div className='form-card'>
+        <form action="#">
+    <input type="text" placeholder='Ismingiz' className='form-card--name'/>
+    <div className='form-card--box'> 
+        <input type="num" placeholder='Telefon nomeringiz' className='form-card--tell'/>
+        <input type="email" placeholder='Pochtangiz'className='form-card--tell' />
+    </div>
+    <button className='form-card--btn'>
+    Yuborish
+    </button>
+    </form>
     </div>
     </div>
         </section>
